d7abd4284c4f58d3ac7c7c125588a908cc1ec378
Mooner92 <chlaudgjs0989@gmail.com>
2022-03-20 15:34:01 +0900

    add

---
+++ Google-220320/A.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    int tc;
+    cin>>tc;
+    for(int i=0;i<tc;i++){
+        string p,q;
+        cin>>p>>q;
+        for(int j=0;j<p.length();){
+            for(int k=0;k<q.length();){
+                if(p[j]==q[k]){
+                    p.erase(j);
+                    q.erase(k);
+                }
+            }
+
+        }
+    }
+}
