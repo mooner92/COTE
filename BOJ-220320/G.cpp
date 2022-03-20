0d0e7cbaebd7a8ebcf4acbe3951e5ed8fb7722a1
Mooner92 <chlaudgjs0989@gmail.com>
2022-03-20 16:31:24 +0900

    add

---
+++ BOJ-220320/G.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int k[50001];
+int l[50001];
+int main(){
+    int tc;
+    cin>>tc;
+    for(int i;i<tc;i++){  //도수 전개로 풀이할 예정
+        int a;
+        cin>>a;
+        
+    }
+}
+//계단은 양수만 가능
+//하나만 있는 수[K]가 있다면 그 수는 꼭짓점이므로 [k-1]은 짝수이어야 함 
+//
