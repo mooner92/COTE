8c364709d1af43f8e6deffe27af167b07bc9e36e
Mooner92 <chlaudgjs0989@gmail.com>
2022-03-20 15:58:09 +0900

    add

--- BOJ-220320/C.cpp
+++ BOJ-220320/C.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 using namespace std;
 int main(){
+    int q,w;
+    cin>>q>>w;   //q,w를 입력받음
+    int** arr = new int*[q];
+    for(int i=0;i<q;i++){
+        arr[i] = new int[w];   //q,w를 사용한 이중배열의 동적할당
+    }
     
 }
