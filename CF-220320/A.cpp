07a82eab69509c6ff986bcd67a52a1b72ef2629c
Mooner92 <chlaudgjs0989@gmail.com>
2022-03-20 22:10:51 +0900

    add

---
+++ CF-220320/A.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main(){
+    int tc;
+    cin>>tc;
+    for(int i=0;i<tc;i++){
+        int q;
+        cin>>q;
+        vector<int> k;
+        for(int j=0;j<q;j++){
+            int h;
+            cin>>h;
+            k.push_back(h); //벡터 만들어줌
+        }
+        stable_sort(k.begin(),k.end());
+        reverse(k.begin(),k.end());
+        cout << k[0]+k[1]<<"\n";
+
+    }
+}
---
+++ CF-220320/B.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int tc;
+    cin>>tc;
+    //bool exit = false;
+    for(int i=0;i<tc;i++){
+        string s;
+        cin>>s;
+        int p = s.size();
+        for(int j=0;j<p;p--){ //standard array position
+            for(int h=0;h<s.size();h++){ //pivot
+                    //같은곳에 있지 않을 때
+                if(j!=h){
+                    if(s[j]==s[h]){     //둘이 같으면
+                        s.erase(0,1); // 그 칸 지워줌
+                        //s[j] = ' \b';
+                        break;
+                    }
+                }
+                if(h==s.size()-1 /*&& s[j]!=s[h]*/){ //끝칸까지 왔고 j항이랑 h항이 다르면
+                    break;
+                }
+                
+            }
+            /*if(exit==true)
+                break;*/
+        }  //같은문자 전부 제거했으면 
+        cout << s<<"\n";
+    }
+}
