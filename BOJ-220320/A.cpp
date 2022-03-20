8f9ccc8acca1e5d4421f0cef917fe9ac5496c2fd
Mooner92 <chlaudgjs0989@gmail.com>
2022-03-20 13:38:16 +0900

    add

---
+++ BOJ-220320/A.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    int q;
+    cin>>q;
+    cout <<pow(2,q);
+}
---
+++ BOJ-220320/B.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+using ll = long long;
+int main(){
+    int tc;
+    cin>>tc;
+    for(int i=1;i<=tc;i++){
+        printf("Material Management %d\n",i);
+        printf("Classification ---- End!\n");
+    }
+}
