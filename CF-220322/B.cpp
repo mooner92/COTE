80dacdc8dffd7fe959febd4b33bddc2e3eceef56
Mooner92 <chlaudgjs0989@gmail.com>
2022-03-23 00:54:44 +0900

    add

--- CF-220322/B.cpp
+++ CF-220322/B.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
 using namespace std;
 int main(){
+    int tc;
+    cin>>tc;
+    for(int i=0;i<tc;i++){
+        int n,b,x,y;  //tmp는 척도로 쓸 예정   sum은 출력값
+        long long tmp=0,sum=0;
+        cin>>n>>b>>x>>y;
+        for(int j=0;j<n;j++){
+            if((tmp+x)>b){   //tmp+x가 b를 초과하는 경우 tmp-y를 실행
+                tmp-=y;
+                sum+=tmp;
+                //cout <<sum<<"\n";
+            }
+            else{
+                tmp+=x;      //tmp+x가 b이하라면 x를 더한 값을 tmp에 저장 후 다시 루프
+                sum+=tmp;
+                //cout <<sum<<"\n";
+            }
+        }
+        cout << sum<<"\n";
+    }
     
 }
---
+++ CF-220322/C.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    int tc;
+    cin>>tc;
+    for(int i=0;i<tc;i++){
+        int n;
+        string s;
+        cin>>n>>s;
+        char k = s[0];  //string 의 첫번째 문자를 k에 저장
+        if(k==')'){ //닫침괄호로 시작하면 완전조합은 만들 수 없음 펠린드롬만 가능
+            
+        }
+
+        
+
+    }
+}
