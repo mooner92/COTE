279ff423f9884e27ad32cab5fbb285eb37f19c70
Mooner92 <chlaudgjs0989@gmail.com>
2022-03-27 23:19:02 +0900

    add

---
+++ BOJ-220327/A.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char n;
+    cin>>n;
+    if(n=='N' || n=='n'){
+        cout<<"Naver D2";
+    }
+    else
+        cout << "Naver Whale";
+}
