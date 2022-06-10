55bc354dd3a7b14a569bef93845ec2386632b868
Mooner92 <chlaudgjs0989@gmail.com>
2022-06-10 16:21:35 +0900

    add

--- BOJ-220610/A.cpp
+++ BOJ-220610/A.cpp
@@ -1,5 +1,54 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+
+double bmi(double a, double b)
+{
+    return b / pow((a / 100), 2);
+}
 int main()
 {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        double h, w;
+        cin >> h >> w;
+        if (h <= 140.0)
+            cout << "6"
+                 << "\n";
+        else if (h > 140.0 && h < 146.0)
+            cout << "5"
+                 << "\n";
+        else if (h >= 146.0 && h < 159.0)
+            cout << "4"
+                 << "\n";
+        else if (h >= 159.0 && h < 161.0)
+        {
+            if (bmi(h, w) < 16.0 || bmi(h, w) >= 35.0)
+                cout << "4"
+                     << "\n";
+            else // if (bmi(h, w) >= 16.0 && bmi(h, w) < 35.0)
+                cout << "3"
+                     << "\n";
+        }
+        else if (h >= 161.0 && h < 204.0)
+        {
+            if (bmi(h, w) < 16.0 || bmi(h, w) >= 35.0)
+                cout << "4"
+                     << "\n";
+            else if ((bmi(h, w) >= 16.0 && bmi(h, w) < 18.5) || (bmi(h, w) > 30.0 || bmi(h, w) < 35.0))
+                cout << "3"
+                     << "\n";
+            else if ((bmi(h, w) >= 18.5 && bmi(h, w) < 20.0) || (bmi(h, w) >= 25.0 || bmi(h, w) < 30.0))
+                cout << "2"
+                     << "\n";
+            else // if ((bmi(h, w) >= 20.0 && bmi(h, w) < 25.0))
+                cout << "1"
+                     << "\n";
+        }
+        else
+            cout << "4"
+                 << "\n";
+    }
 }
