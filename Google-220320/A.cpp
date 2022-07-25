ec40c3b2386b55642a416894ec5731a392515e92
Mooner92 <chlaudgjs0@naver.com>
2022-07-26 00:00:04 +0900

    add

--- Google-220320/A.cpp
+++ Google-220320/A.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int main(){
+int main()
+{
     int tc;
-    cin>>tc;
-    for(int i=0;i<tc;i++){
-        string p,q;
-        cin>>p>>q;
-        for(int j=0;j<p.length();){
-            for(int k=0;k<q.length();){
-                if(p[j]==q[k]){
+    cin >> tc;
+    for (int i = 0; i < tc; i++)
+    {
+        string p, q;
+        cin >> p >> q;
+        for (int j = 0; j < p.length();)
+        {
+            for (int k = 0; k < q.length();)
+            {
+                if (p[j] == q[k])
+                {
                     p.erase(j);
                     q.erase(k);
-                }
+                } /**/
             }
-
         }
     }
 }
