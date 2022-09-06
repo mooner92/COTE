d1df0f13f4e39de4568854d98db67489f54c3748
Mooner92 <chlaudgjs0989@gmail.com>
2022-09-06 09:05:52 +0900

    add

---
+++ BOJ-220905/A.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int x, y, k, n;
+    for (int i = 0; i < n; i++)
+    {
+    }
+}
