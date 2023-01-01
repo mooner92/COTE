7b8439fc3a84f2223931657d5a4405a48730b47d
Mooner92 <chlaudgjs0989@gmail.com>
2023-01-01 16:55:36 +0900

    add

--- BOJ-230101/Sci-C.cpp
+++ BOJ-230101/Sci-C.cpp
@@ -1,13 +1,44 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <stdlib.h>
 using namespace std;
 
 int main()
 {
+    vector<int> v;
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int n, x, y, sum = 0, nsum = 0;
-    cin >> n;
-    for (int i = 0; i < n; i++)
+    int n, x, y, sum = 0, nsum = 0, k, u;
+    cin >> n >> x >> y;
+    for (int i = 0; i < n; i++) //(n*(n-1))/2
     {
+        cin >> k;
+        v.push_back(k);
     }
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            u = abs(v[i] - v[j]);
+            if (v[i] != v[j])
+            {
+                sum += y;
+                u = (u / x) * y;
+                if (i == 0)
+                {
+                    if (v[i] < v[j])
+                    {
+                        nsum += u + y;
+                    }
+                    nsum %= 998244353;
+                }
+
+                sum += u;
+                sum %= 998244353;
+            }
+        }
+    }
+    cout << sum << " " << nsum;
 }
