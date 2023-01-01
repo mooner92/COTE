563d61640e3060ca748f26ece50fd76f02efe10f
Mooner92 <chlaudgjs0989@gmail.com>
2023-01-01 22:56:44 +0900

    add 송년대회

---
+++ BOJ-230101_1/A.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <utility>
+using namespace std;
+int main()
+{
+    int n, k, x, q, w, max = -99999;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    vector<pair<int, int>> v;
+    cin >> n >> k >> x;
+    while (n--)
+    {
+        cin >> q >> w;
+        v.push_back(make_pair(q, w));
+    }
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (v[i].)
+        }
+    }
+    cout << pow(x, k);
+}
