b8f573acea77b3368eeae6e7cac02471a65c277d
Mooner92 <chlaudgjs0989@gmail.com>
2023-05-22 15:03:28 +0900

    add

---
+++ BOJ-230521/A.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    int num = 0;
+    char cs[5] = {'M', 'O', 'B', 'I', 'S'};
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < s.size(); j++)
+        {
+            if (cs[i] == s[j])
+            {
+                num++;
+                break;
+            }
+        }
+    }
+    if (num == 5)
+        cout << "YES";
+    else
+        cout << "NO";
+} ///
