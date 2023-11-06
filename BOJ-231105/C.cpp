3f54252d668c6b104018dfb5136c1f04166a6c3a
Mooner92 <54096383+mooner92@users.noreply.github.com>
2023-11-06 12:27:44 +0900

    add

---
+++ BOJ-231105/C.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    for (int i = 0; i < k - 1; i++)
+    {
+        cout << 1;
+    }
+    cout << n;
+}
+///
