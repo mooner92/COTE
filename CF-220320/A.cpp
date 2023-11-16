41696d398cc5ecf3540e4d26ac0f79b8e61644a6
Mooner92 <54096383+mooner92@users.noreply.github.com>
2023-11-17 01:43:16 +0900

    add

--- CF-220320/A.cpp
+++ CF-220320/A.cpp
@@ -2,21 +2,24 @@
 #include <algorithm>
 #include <vector>
 using namespace std;
-int main(){
+int main()
+{
     int tc;
-    cin>>tc;
-    for(int i=0;i<tc;i++){
+    cin >> tc;
+    for (int i = 0; i < tc; i++)
+    {
         int q;
-        cin>>q;
+        cin >> q;
         vector<int> k;
-        for(int j=0;j<q;j++){
+        for (int j = 0; j < q; j++)
+        {
             int h;
-            cin>>h;
-            k.push_back(h); //벡터 만들어줌
+            cin >> h;
+            k.push_back(h); // 벡터 만들어줌
         }
-        stable_sort(k.begin(),k.end());
-        reverse(k.begin(),k.end());
-        cout << k[0]+k[1]<<"\n";
-
+        stable_sort(k.begin(), k.end());
+        reverse(k.begin(), k.end());
+        cout << k[0] + k[1] << "\n";
     }
 }
+////
